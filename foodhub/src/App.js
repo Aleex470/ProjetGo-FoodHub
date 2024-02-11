a176e502328768d247a8982a772c3dedb4763834
Aleex470 <alexvisage2001@gmail.com>
2024-02-11 12:59:36 +0100

    condition de connection pour admin ou homepage

--- foodhub/src/App.js
+++ foodhub/src/App.js
@@ -1,13 +1,26 @@
+import React, { useState } from 'react';
 import './App.css';
 import HomePage from './components/HomePage';
 import AdminPage from './components/AdminPage';
 
-
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <div className="App">
-        {/* <HomePage/> */}
-        <AdminPage />
+      {isLoggedIn ? (
+        <AdminPage onLogout={handleLogout} />
+      ) : (
+        <HomePage onLogin={handleLogin} />
+      )}
     </div>
   );
 }
